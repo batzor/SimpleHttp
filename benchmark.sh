b7a4c976b55aa5ddeea92d35d642849863ecf930
qvntm <batzorig1691@gmail.com>
2020-05-26 00:15:14 +0900

    Remove time measurement

--- benchmark.sh
+++ benchmark.sh
@@ -4,7 +4,7 @@ make clean
 make test
 ./http_server &
 mkdir tmp
-time $(seq 1 $1 | xargs -n1 -P$1 bash -c 'i=$0; url="http://localhost:8000/index.html"; filename="tmp/${i}.html"; curl -s --http1.1 -o $filename $url')
-echo Downloaded $(ls tmp | wc -l) out of $1
+seq 1 1000 | xargs -n1 -P1000 bash -c 'i=$0; url="http://localhost:8000/index.html"; filename="tmp/${i}.html"; curl -s --http1.1 -o $filename $url'
+echo Downloaded $(ls tmp | wc -l) out of 1000
 rm -rf tmp
 
