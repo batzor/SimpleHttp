22310368ea54efb45508169a705acd8dc4e0cefd
qvntm <batzorig1691@gmail.com>
2020-06-17 20:00:53 +0900

    Optimize benchmark script

--- benchmark.sh
+++ benchmark.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mkdir tmp
-time $(seq 1 10000 | xargs -n1 -P100 bash -c 'i=$0; url="localhost:8080/index.html"; filename="tmp/${i}.html"; wget --quiet -O $filename $url')
+time $(seq 1 10000 | xargs -n1 -P100 bash -c 'i=$0; url="localhost:8080/index.html"; filename="tmp/${i}.html"; wget --quiet --no-dns-cache --no-hsts -O $filename $url')
 echo Downloaded $(ls tmp | wc -l) out of 10000
 rm -rf tmp
 
