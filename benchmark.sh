c2d1b32e9361a6d873a009620a944effe53fa19b
qvntm <batzorig1691@gmail.com>
2020-06-17 17:25:34 +0900

    Improve benchmark script

--- benchmark.sh
+++ benchmark.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mkdir tmp
-time $(seq 1 10000 | xargs -n1 -P100 bash -c 'i=$0; url="localhost:8080/index.html"; filename="tmp/${i}.html"; wget --quiet -O $filename $url')
-echo Downloaded $(ls tmp | wc -l) out of 10000
+time $(seq 1 100 | xargs -n1 -P1 bash -c 'i=$0; url="localhost:8080/index.html"; filename="tmp/${i}.html"; wget --quiet --no-hsts --no-dns-cache -O $filename $url')
+echo Downloaded $(ls tmp | wc -l) out of 100
 rm -rf tmp
 
